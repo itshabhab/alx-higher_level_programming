0ec223d1ae1260c72273391d31ea9f821eb21465
itshabhab <root@LAPTOP-RA6CILPB>
2022-06-30 09:59:06 +0300

    past the deadline

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+def main():
+argc = len(sys.argv) - 1
+if argc == 0:
+print("0 arguments.")
+elif argc == 1:
+print("1 argument:")
+print("1: {}".format(sys.argv[1]))
+else:
+print("{:d} arguments:".format(argc))
+for count in range(1, argc + 1):
+print("{}: {}".format(count, sys.argv[count])
+if __name__ == "__main__":
+main()
